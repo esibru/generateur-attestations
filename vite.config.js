54d9be9b1e7ac78bf66b28afce5caae3c6085f5c
jlechien <jlechien@he2b.be>
2025-08-21 14:23:23 +0200

    mise à jour configuration

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 
 export default defineConfig({
-    server: {
-        allowedHosts: ["nri.intranet.he2b.be"] // defined locally in my /etc/hosts file
-    },
     plugins: [vue()],
     base: '/generateur-attestations'  
 })
